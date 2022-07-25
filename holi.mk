d9e99ca686cf8a2cd8e8e2abed37d791d0570e7b
Dragon-1237 <89452912+Dragon-1237@users.noreply.github.com>
2022-07-25 20:12:56 +0530

    Update holi.mk

--- holi.mk
+++ holi.mk
@@ -519,4 +519,4 @@ PRODUCT_BOOT_JARS += \
     WfdCommon
 
 # Inherit the proprietary files
-$(call inherit-product, vendor/xiaomi/veux/sm6375-common/sm6375-common-vendor.mk)
+$(call inherit-product, vendor/xiaomi/sm6375-common/sm6375-common-vendor.mk)
